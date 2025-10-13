400a673b78d7f6107c0185ca91375acda814241c
Guilherme Moraes <guimoraes.dev@gmail.com>
2025-10-13 18:29:27 -0300

    refactor: simplify query formatting in GET /api/status for improved readability

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -3,24 +3,20 @@ import { databaseModel } from "@models/database";
 export async function GET() {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await databaseModel.query(`
-    SHOW server_version;
-  `);
+  const databaseVersionResult = await databaseModel.query(
+    "SHOW server_version;",
+  );
   const databaseVersionValue = databaseVersionResult.rows[0]?.server_version;
 
-  const databaseMaxConnectionsResult = await databaseModel.query(`
-    SHOW max_connections;
-  `);
+  const databaseMaxConnectionsResult = await databaseModel.query(
+    "SHOW max_connections;",
+  );
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0]?.max_connections;
 
   const databaseName = process.env.POSTGRES_DB || "postgres";
   const databaseActivities = await databaseModel.query(
-    `
-    SELECT COUNT(*)::int
-    FROM pg_stat_activity
-    WHERE datname = $1;
-  `,
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
     [databaseName],
   );
   const databaseOpenedConnectionsValue = databaseActivities.rows[0]?.count;
