4c4c70f4ef45e04a0981cb2afa5979f3d7db0522
Guilherme Moraes <guimoraes.dev@gmail.com>
2025-10-14 09:12:00 -0300

    fix: add SSL configuration to database client for production environment

--- src/models/database.ts
+++ src/models/database.ts
@@ -10,6 +10,10 @@ const query = async (
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
   });
 
   try {
